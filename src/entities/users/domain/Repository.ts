808893ef6079ae54b2bd8ba83c534b3e55b5808c
IsaacNavarro <driveraoriginal@outlook.com>
2023-12-28 17:00:29 -0600

    update

--- src/entities/users/domain/Repository.ts
+++ src/entities/users/domain/Repository.ts
@@ -1,7 +1,11 @@
-import { injectable } from 'tsyringe';
+import { autoInjectable } from 'tsyringe';
+import UserModel from './Model';
 
-@injectable()
+@autoInjectable()
 export default class Repository {
-  constructor(private model: Model) {}
-  create() {}
+  constructor(private model: UserModel) {}
+  create<P>(args: P) {
+    const self = this;
+    const result = self.model.create();
+  }
 }
